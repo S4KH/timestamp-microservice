b0ceab5980a44dda9cd5288632e219f8ed7b23cf
s4kh <kh.syerikjan@gmail.com>
2016-02-01 23:58:37 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var moment = require('moment')
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+//Showing index html for calls without parameter
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'))
+});
+
+app.get('/:times', function(req, res){
+    var times = req.params.times
+    var d
+    var result = {'unix':null, 'natural':null}
+    if(/^\d{8,}$/.test(times)) {
+        d = moment(times, "X");
+    } else {
+        d = moment(times, "MMMM D, YYYY");
+    }
+    if(d.isValid()) {
+        result.unix = d.format("X")
+        result.natural = d.format("MMMM D, YYYY")
+    }
+    res.json(result)
+});
+
+var port = 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
